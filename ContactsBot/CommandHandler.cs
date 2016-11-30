99b317a3a9affb5e6b510a880c4a60361f952513
ObsidianMinor <obsidianminor@gmail.com>
2016-11-30 12:17:14 -0600

    Complete merge, denied changes to help command

---
+++ ContactsBot/CommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Linq;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace ContactsBot
+{
+    public class CommandHandler
+    {
+        private CommandService _commands;
+        private DiscordSocketClient _client;
+        private BotConfiguration _config;
+        IDependencyMap _map;
+
+        public async Task Install(IDependencyMap map)
+        {
+            _map = map;
+            _client = _map.Get<DiscordSocketClient>();
+            _config = _map.Get<BotConfiguration>();
+            _commands = new CommandService();
+            _map.Add(_commands);
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += HandleCommand;
+        }
+
+        private async Task HandleCommand(SocketMessage msg)
+        {
+            var message = msg as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (message.HasCharPrefix(_config.PrefixCharacter, ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new CommandContext(_client, message);
+
+                // Provide the dependency map when executing commands
+                var result = await _commands.ExecuteAsync(context, argPos, _map);
+                if (!result.IsSuccess)
+                {
+                    await message.Channel.SendMessageAsync("```" + ((ExecuteResult)result).Exception.ToString() + "```");
+                }
+            }
+        }
+    }
+}
--- ContactsBot/Program.cs
+++ ContactsBot/Program.cs
@@ -1,6 +1,4 @@
-﻿using Contacts;
-using ContactsBot;
-using Discord.API;
+﻿using Discord.API;
 using Discord.API.Rest;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -11,128 +9,67 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
-class Program
+namespace ContactsBot
 {
-    static void Main(string[] args) => new Program().RunBot().GetAwaiter().GetResult();
-
-    DiscordSocketClient _client;
-    CommandService _commands;
-    DependencyMap _map;
-    BotConfiguration config;
-
-    public async Task RunBot()
+    class Program
     {
-        if (!File.Exists("config.json"))
+        static void Main(string[] args) => new Program().RunBot().GetAwaiter().GetResult();
+
+        DiscordSocketClient _client;
+        DependencyMap _map;
+        BotConfiguration _config;
+        CommandHandler _handler;
+        AntiAdvertisement _antiAdvertisement;
+
+        public async Task RunBot()
         {
-            Console.Write("Please enter a bot token: ");
-            string token = Console.ReadLine();
-            config = BotConfiguration.CreateBotConfigWithToken("config.json", token);
-        }
-        else
-        {
-            config = BotConfiguration.ProcessBotConfig("config.json");
-#if DEV
-            if(string.IsNullOrWhiteSpace(config.DevToken))
+            if (!File.Exists("config.json"))
             {
-                Console.Write("Please enter a dev token: ");
+                Console.Write("Please enter a bot token: ");
                 string token = Console.ReadLine();
-                config.DevToken = token;
-                config.SaveBotConfig("config.json");
-#endif
+                _config = BotConfiguration.CreateBotConfigWithToken("config.json", token);
             }
+            else
+                _config = BotConfiguration.ProcessBotConfig("config.json");
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                AudioMode = Discord.Audio.AudioMode.Disabled,
+                LogLevel = Discord.LogSeverity.Debug
+            });
+
+            // Create the dependency map and inject the client and config into it
+            _map = new DependencyMap();
+            _map.Add(_client);
+            _map.Add(_config);
+
+            _handler = new CommandHandler();
+            await _handler.Install(_map);
+
+            _antiAdvertisement = new AntiAdvertisement();
+            _antiAdvertisement.Install(_map);
+            _antiAdvertisement.Enable();
+
+            _client.Log += _client_Log; // console info
+
+            await _client.LoginAsync(Discord.TokenType.Bot, _config.Token);
+            await _client.ConnectAsync();
+
+            await _client.SetGame("Helping you C#");
+
+            await Task.Delay(-1);
         }
 
-        _client = new DiscordSocketClient(new DiscordSocketConfig
+        private Task _client_Log(Discord.LogMessage arg)
         {
-            AudioMode = Discord.Audio.AudioMode.Disabled,
-            LogLevel = Discord.LogSeverity.Debug
-        });
-        _commands = new CommandService();
-        // Create the dependency map and inject the client and commands service into it
-        _map = new DependencyMap();
-        _map.Add(_client);
-        _map.Add(_commands);
-
-        await ApplyCommands();
-        
-        _client.Log += _client_Log; // console info
-        _client.MessageReceived += _client_MessageReceived; // filtering and commands
-
-#if DEV
-        await _client.LoginAsync(Discord.TokenType.Bot, config.DevToken);
-#else
-        await _client.LoginAsync(Discord.TokenType.Bot, config.Token);
-#endif
-
-        await _client.ConnectAsync();
-
-#if DEV
-        if(File.Exists("Assets/contacts-dev.png"))
-            await _client.CurrentUser.ModifyAsync((ModifyCurrentUserParams mod) => 
+            switch (arg.Severity)
             {
-                mod.Avatar = new Image(new FileStream("Assets/contacts-dev.png", FileMode.Open));
-            });
-#else
-        if (File.Exists("Assets/contacts.png"))
-            await _client.CurrentUser.ModifyAsync((ModifyCurrentUserParams mod) =>
-            {
-                mod.Avatar = new Image(new FileStream("Assets/contacts.png", FileMode.Open));
-            });
-#endif
-
-        await _client.SetGame("Helping you C#");
-
-        await Task.Delay(-1);
-    }
-
-    private async Task _client_Log(Discord.LogMessage arg)
-    {
-        Console.WriteLine(arg.ToString());
-    }
-
-    private async Task ApplyCommands()
-    {
-        await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
-    } 
-
-    private async Task _client_MessageReceived(SocketMessage msg)
-    {
-        var message = msg as SocketUserMessage;
-        if (message == null) return;
-
-#if DEV
-        if (!string.Equals(message.Channel.Name, "Contacts", StringComparison.OrdinalIgnoreCase))
-            return;
-#endif
-
-        int argPos = 0;
-
-        if(message.HasCharPrefix(config.PrefixCharacter, ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
-        {
-            var context = new CommandContext(_client, message);
-
-            // Provide the dependency map when executing commands
-            var result = await _commands.ExecuteAsync(context, argPos, _map);
-            if (!result.IsSuccess && result.ErrorReason != "Unknown command.")
-                await message.Channel.SendMessageAsync($"{result.ErrorReason}: {((ExecuteResult)result).Exception.Source}");
-        }
-        else
-        {
-            var authorAsGuildUser = message.Author as SocketGuildUser;
-            if (authorAsGuildUser == null) return;
-
-            var roles = authorAsGuildUser.Guild.Roles;
-            string role = roles.First(r => authorAsGuildUser.RoleIds.Any(gr => r.Id == gr)).Name;
-
-            if (!Moderation.StandardRoles.Any(v => v == role))
-            {
-                if (message.Content.Contains("https://discord.gg/"))
-                {
-                    await message.Channel.DeleteMessagesAsync(new[] { message });
-                    var dmChannel = await authorAsGuildUser.CreateDMChannelAsync();
-                    await dmChannel.SendMessageAsync("Your Discord invite link was removed. Please ask a staff member or regular to post it.");
-                }
+                case Discord.LogSeverity.Error: Console.ForegroundColor = ConsoleColor.Red; break;
+                case Discord.LogSeverity.Warning: Console.ForegroundColor = ConsoleColor.Yellow; break;
             }
+            Console.WriteLine(arg.ToString());
+            Console.ForegroundColor = ConsoleColor.Gray;
+            return Task.CompletedTask;
         }
     }
 }
