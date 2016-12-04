b8b142ed23c2c353af6f18b677e6ee5d6d02ea72
Mathias Storm <mathiasstorm1@gmail.com>
2016-12-04 03:18:51 +0100

    Adding advanced url inspection in antiadvertising module

--- ContactsBot/Modules/AntiAdvertisement.cs
+++ ContactsBot/Modules/AntiAdvertisement.cs
@@ -6,6 +6,9 @@ using Discord;
 using Discord.WebSocket;
 using Discord.Commands;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using System.Net;
 
 namespace ContactsBot.Modules
 {
@@ -39,13 +42,45 @@ namespace ContactsBot.Modules
 
             if (!authorAsGuildUser.IsCorrectRole(guildChannel.Guild, Moderation.StandardRoles))
             {
-                if (message.Content.Contains("discord.gg/"))
+                if (containsInviteLink(message.Content))
                 {
                     await message.Channel.DeleteMessagesAsync(new[] { message });
                     var dmChannel = await authorAsGuildUser.CreateDMChannelAsync();
                     await dmChannel.SendMessageAsync("Your Discord invite link was removed. Please ask a staff member or regular to post it.");
                 }
             }
+        }
+        
+        private bool containsInviteLink(string message)
+        {
+            var urlRegex = new Regex(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.IgnoreCase);
+            var matches = urlRegex.Matches(message);
+            var requests = new List<Task<bool>>();
+
+            foreach(Match match in matches)
+            {
+                if (match.Groups[0].Value.Contains("discord.gg"))
+                {
+                    return true;
+                }
+                requests.Add(isHiddenInvite(match.Groups[0].Value));
+            }
+
+            Task.WaitAll(requests.ToArray());
+
+            return requests.Where(x => x.Result).Count() != 0;
+        }
+
+        private async Task<bool> isHiddenInvite(string link)
+        {
+            var client = new HttpClient();
+
+            var res = await client.GetAsync(link, HttpCompletionOption.ResponseHeadersRead);
+            if (res.StatusCode == HttpStatusCode.OK)
+            {
+                return res.RequestMessage.RequestUri.AbsoluteUri.Contains("discordapp.com/invite");
+            }
+            return false;
         }
     }
 }
