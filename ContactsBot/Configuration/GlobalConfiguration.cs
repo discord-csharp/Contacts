9af60f7fe25aa3618b1be10139e1f9c050192fe3
ObsidianMinor <obsidianminor@gmail.com>
2017-01-01 17:04:57 -0600

    Revert "Doing work on configuration data and logging to make it customizable between servers"
    This reverts commit 12854e3377711ac5daf5abcad4b0d15a59dcfba0.

--- ContactsBot/Configuration/GlobalConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ContactsBot.Configuration
-{
-    public class GlobalConfiguration
-    {
-        public string Token { get; set; } = "";
-        public string Game { get; set; } = "Helping you C#";
-    }
-}
