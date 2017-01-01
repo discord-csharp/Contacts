12854e3377711ac5daf5abcad4b0d15a59dcfba0
ObsidianMinor <obsidianminor@gmail.com>
2017-01-01 12:08:54 -0600

    Doing work on configuration data and logging to make it customizable between servers

---
+++ ContactsBot/Configuration/GlobalConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactsBot.Configuration
+{
+    public class GlobalConfiguration
+    {
+        public string Token { get; set; } = "";
+        public string Game { get; set; } = "Helping you C#";
+    }
+}
---
+++ ContactsBot/MessageActions/IBotService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsBot.Modules
+{
+    public interface IBotService
+    {
+        bool Enabled { get; }
+
+        bool Enable();
+
+        bool Disable();
+    }
+}
